af80ac1d1d2ac21e84e12201773c906c87d057ee
KathCardenas <kath9704.gmail.com>
2024-06-21 12:12:06 -0600

    feat: Añadir base de proyecto en commit inicial

---
+++ src/main/java/com/ejemplo/apirest/ejemplo_apirest/EjemploApirestApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.apirest.ejemplo_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/apirest/ejemplo_apirest/EjemploApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.apirest.ejemplo_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
